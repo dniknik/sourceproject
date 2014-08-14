487bc3f99acfcaccab42d01c923c6dd7346d6c5e
dnn.home <dniknik@gmail.com>
2014-08-14 05:40:34 +0400

    test ssh auth

--- src/controller/TreeNodeController.class.php
+++ src/controller/TreeNodeController.class.php
@@ -3,10 +3,8 @@
 lmb_require('limb/cms/src/controller/lmbObjectController.class.php');
 lmb_require('limb/cms/src/model/TreeNode.class.php');
 lmb_require('limb/cms/src/model/lmbCmsDocument.class.php');
-//lmb_require('limb/cms/src/model/Seo.class.php');
 
 class TreeNodeController extends lmbObjectController
-//class DocumentController extends lmbObjectController
 {
   protected $_object_class_name = 'TreeNode';
   //protected $_object_class_name = 'lmbCmsDocument';
