4e2be1b7e7c5bd64788086845e009e091ef74f10
dnn.home <dniknik@gmail.com>
2014-08-14 05:47:20 +0400

    test ssh auth with canges2

--- src/controller/Controller.class.php
+++ src/controller/Controller.class.php
@@ -6,11 +6,10 @@ lmb_require('limb/util/system/lmbFs.class.php');
 lmb_require('limb/tree/src/lmbTreeDecorator.class.php');
 lmb_require('limb/tree/src/lmbMPTree.class.php');
 
-lmb_require('limb/datetime/src/lmbDate.class.php');
-lmb_require('limb/datetime/src/lmbDateTime.class.php');
-lmb_require('limb/datetime/src/lmbDateTimeZone.class.php');
+//lmb_require('limb/datetime/src/lmbDate.class.php');
+//lmb_require('limb/datetime/src/lmbDateTime.class.php');
+//lmb_require('limb/datetime/src/lmbDateTimeZone.class.php');
 
-//lmb_require('limb/search/src/controller/SearchController.class.php');
 lmb_require('src/helper/AlphabetHelper.class.php');
 
 class Controller extends lmbController
